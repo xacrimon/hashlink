44b35257ac2dcc878ae3af98c5da9d49a5d46743
Joel Wejdenstål <joel.wejdenstal@gmail.com>
2025-04-26 08:54:36 +0200

    force inlining through hash wrapper as not inlining is always worse in every regard

--- src/lib.rs
+++ src/lib.rs
@@ -22,7 +22,7 @@ pub struct DefaultHashBuilder(hashbrown::DefaultHashBuilder);
 impl BuildHasher for DefaultHashBuilder {
     type Hasher = DefaultHasher;
 
-    #[inline]
+    #[inline(always)]
     fn build_hasher(&self) -> Self::Hasher {
         DefaultHasher(self.0.build_hasher())
     }
@@ -33,73 +33,73 @@ impl BuildHasher for DefaultHashBuilder {
 pub struct DefaultHasher(<hashbrown::DefaultHashBuilder as BuildHasher>::Hasher);
 
 impl Hasher for DefaultHasher {
-    #[inline]
+    #[inline(always)]
     fn write(&mut self, bytes: &[u8]) {
         self.0.write(bytes)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_u8(&mut self, i: u8) {
         self.0.write_u8(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_u16(&mut self, i: u16) {
         self.0.write_u16(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_u32(&mut self, i: u32) {
         self.0.write_u32(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_u64(&mut self, i: u64) {
         self.0.write_u64(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_u128(&mut self, i: u128) {
         self.0.write_u128(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_usize(&mut self, i: usize) {
         self.0.write_usize(i)
     }
 
-    #[inline]
-    fn finish(&self) -> u64 {
-        self.0.finish()
-    }
-
-    #[inline]
+    #[inline(always)]
     fn write_i8(&mut self, i: i8) {
         self.0.write_i8(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_i16(&mut self, i: i16) {
         self.0.write_i16(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_i32(&mut self, i: i32) {
         self.0.write_i32(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_i64(&mut self, i: i64) {
         self.0.write_i64(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_i128(&mut self, i: i128) {
         self.0.write_i128(i)
     }
 
-    #[inline]
+    #[inline(always)]
     fn write_isize(&mut self, i: isize) {
         self.0.write_isize(i)
+    }
+
+    #[inline(always)]
+    fn finish(&self) -> u64 {
+        self.0.finish()
     }
 }
